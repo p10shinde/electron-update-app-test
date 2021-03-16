757d83e839e613aaff8dcbbf36f3b30bc7805e7d
p10shinde <p10shinde@gmail.com>
2021-03-16 11:56:29 +0530

    test commit

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { app, BrowserWindow, screen } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
-
+require('update-electron-app')()
 let win: BrowserWindow = null;
 const args = process.argv.slice(1),
   serve = args.some(val => val === '--serve');
